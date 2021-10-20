6266375c07eb7df4d1c3193800b50e878607f259
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-20 11:48:26 +0200

    Spring Boot Initialization

---
+++ src/test/java/com/bosonit/springboot/rs1/Rs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.rs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
